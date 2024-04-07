995628d38972cb99d2552e3693c6406d14fb0cc2
ayakamisaka1 <2521845542@qq.com>
2024-04-07 21:01:50 +0800

    first commit

---
+++ src/test/java/com/mq/rockerMqProject/RockerMqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mq.rockerMqProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockerMqProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
